91401bc8eb644ce876c626942ab3510f96202696
rodionov ruslan <rodionov.R.l@gmail.com>
2022-11-03 14:09:31 +0300

    Домашка 2 - использование трёхмерного массива Object вместо String + Изменение размера массива. + Домашка 3 - принципы ООП

---
+++ src/ru/otus/hws/hw03/HomeWorkOop.java
@@ -0,0 +1,10 @@
+package ru.otus.hws.hw03;
+
+public class HomeWorkOop {
+    public static void main(String[] args) {
+        Questions question1 = new Questions();
+        question1.question = "Не лает, не кусает, а в дом не пускает";
+
+        question1.printQuestion();
+    }
+}
