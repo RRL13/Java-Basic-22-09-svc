192fcdadd75599f728051d589897f34b3280b87b
rodionov ruslan <rodionov.R.l@gmail.com>
2022-11-04 16:26:23 +0300

    Домашка 2 - использование трёхмерного массива Object вместо String + Изменение размера массива. + Домашка 3 - принципы ООП

---
+++ src/ru/otus/hws/hw03/Question.java
@@ -0,0 +1,99 @@
+package ru.otus.hws.hw03;
+
+import java.util.Scanner;
+
+class Question {
+    private String question;
+    private String[] answersArray = new String [0];
+    private int rightAnswer = 0;
+
+    public Question() {
+    }
+
+    public Question(String question, int rightAnswer) {
+        this.question = question;
+        this.rightAnswer = rightAnswer;
+    }
+
+    public Question(String question, String[] answersArray, int rightAnswer) {
+        this.question = question;
+        this.answersArray = answersArray;
+        this.rightAnswer = rightAnswer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswersArray(String[] newAnswersArray ){
+        this.answersArray = newAnswersArray;
+    }
+
+    public String[] getAnswersArray() {
+        return answersArray;
+    }
+
+    public int numAnswers(){
+        return this.answersArray.length;
+    }
+
+    public void setRightAnswer(int rightAnswer){
+        this.rightAnswer = rightAnswer;
+    }
+
+    public int getRightAnswer(){
+        return rightAnswer;
+    }
+
+    public void addAnswerVar(String newAnswerVar) {
+        this.answersArray = addElementInStringArray(answersArray, newAnswerVar);
+    }
+
+    public void printQuestion() {
+        System.out.println("Внимание, вопрос: \n" + question);
+    }
+
+    public void printAnswersVars() {
+        int i = 1;
+        for (String answer : answersArray) {
+            System.out.println(i++ + ". " + answer);
+        }
+    }
+
+    public int whatUserSays(){
+        return scanAnswerOk();
+    }
+
+    public boolean isUserRightOrNot(){
+        return scanAnswerOk() == this.rightAnswer;
+    }
+    private int scanAnswerOk() {
+        Scanner scanAnswer = new Scanner(System.in);
+        boolean isAnswerOk = false;
+        int inputUserAnswer = 0;
+        System.out.print("Введите ваш вариант ответа: ");
+        if (scanAnswer.hasNextInt()) {
+            int inputAnswer = scanAnswer.nextInt();
+            isAnswerOk = (inputAnswer > 0 && inputAnswer <= answersArray.length);
+            if (!isAnswerOk) {
+                inputUserAnswer = -1;
+                System.out.println("Вы не попали в диапазон ответов (от 1 до " + answersArray.length + " ). Засчитаем, как неверный ответ.");
+            }
+
+        } else {
+            System.out.println("Вы ввели что-то непотребное. Засчитаем, как неверный ответ.");
+        }
+        return inputUserAnswer;
+    }
+
+    private static String[] addElementInStringArray(String[] inputStringArr, String newElementText){
+        String[] returnStringArr = new String[(inputStringArr.length+1)];
+        System.arraycopy(inputStringArr, 0, returnStringArr, 0, inputStringArr.length);
+        returnStringArr[returnStringArr.length-1] = newElementText;
+        return returnStringArr;
+    }
+}
