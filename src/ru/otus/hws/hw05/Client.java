fa7d37b1e63c3516b033f64f5dec92f7e3692adf
rodionov ruslan <rodionov.R.l@gmail.com>
2022-11-24 12:57:36 +0300

    Домашка5. Долго ломал голову над структурой. В итоге решил сделать сначала просто работающий код, который буду причёсывать уже после. По крайней мере научился пользоваться (скорее просто пощупал) HashMap и ArrayList.

---
+++ src/ru/otus/hws/hw05/Client.java
@@ -0,0 +1,52 @@
+package ru.otus.hws.hw05;
+
+import java.util.Objects;
+import java.util.UUID;
+
+class Client {
+    //private UUID id = UUID.randomUUID();
+    private String name = "";
+    private int age = 0;
+
+    public Client() {};
+
+    public Client(String name, int age) {
+        this.name = name;
+        this.age = age;
+    };
+
+
+//    public UUID getId() {
+//        return id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null)  return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return getAge() == client.getAge() && getName().equals(client.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+}
