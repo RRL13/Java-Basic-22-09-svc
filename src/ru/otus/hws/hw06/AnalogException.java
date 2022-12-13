3b2f522365f6a3e1842634554d9eb976d30b3c6b
rodionov ruslan <rodionov.R.l@gmail.com>
2022-12-13 20:26:02 +0300

    Домашка 5 по обработке ошибок.

--- src/ru/otus/hws/hw06/AnalogException.java
+++ src/ru/otus/hws/hw06/AnalogException.java
@@ -1,6 +1,6 @@
 package ru.otus.hws.hw06;
 
-public class AnalogException extends Exception {
+public class AnalogException extends RuntimeException {
 
     public AnalogException(String message, int lowLevel, int hiLevel, int address, int value) {
         System.out.printf("Имитация записи логфайла. Ошибка:%s. Диапазон: от %d до %d. Адрес: %d. Значение: %d%n", message, lowLevel, hiLevel, address, value);
